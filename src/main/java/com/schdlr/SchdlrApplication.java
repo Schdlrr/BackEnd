f54e330caf778adedcd39c32743c6a4e48fa0d6e
ErdiSyla <erdisyla6@gmail.com>
2024-11-06 05:13:19 +0100

    Added base running application class

---
+++ src/main/java/com/schdlr/SchdlrApplication.java
@@ -0,0 +1,12 @@
+package com.schdlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchdlrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchdlrApplication.class, args);
+    }
+}
