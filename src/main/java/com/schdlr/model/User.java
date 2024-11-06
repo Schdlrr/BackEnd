007d40a1e339dcadc8448c02c910126a24b747e6
ErdiSyla <erdisyla6@gmail.com>
2024-11-06 20:14:16 +0100

    User model finished with id autogeneration(every id has 50 between em)

--- src/main/java/com/schdlr/model/User.java
+++ src/main/java/com/schdlr/model/User.java
@@ -1,15 +1,26 @@
 package com.schdlr.model;
 
-/* import jakarta.persistence.Entity; */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-/* @Entity */
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_seq")
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String contactInfo;
+    private String password;
 }
