baf198134592514dd393547dde82bd5c6175f38e
ErdiSyla <erdisyla6@gmail.com>
2024-11-06 20:50:15 +0100

    Added signin and a development time only get method

--- src/main/java/com/schdlr/controller/UserManagmentController.java
+++ src/main/java/com/schdlr/controller/UserManagmentController.java
@@ -1,9 +1,12 @@
 package com.schdlr.controller;
 
+import java.util.List;
+
 import com.schdlr.model.ResponseObject;
 import com.schdlr.model.User;
 import com.schdlr.service.UserManagmentService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,18 @@ public class UserManagmentController {
         this.service = service;
     }
 
-    @PostMapping
+    @GetMapping
+    public List<User> getUsers(){
+        return service.getUsers();
+    }
+
+    @PostMapping("/signup")
     public ResponseObject userSignUp(@RequestBody User user){
        return service.userSignUp(user);
     }
 
+    @PostMapping("/signin")
+    public ResponseObject userSignIn(@RequestBody User user){
+        return service.userSignIn(user);
+    }
 }
