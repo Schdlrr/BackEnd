df42ed6f9d54f167e8c2bdad810b7250a88d4cef
ErdiSyla <erdisyla6@gmail.com>
2024-11-06 20:52:50 +0100

    Changed return type of findByUsername from User to Optional<User> and added findByPassword for signins

--- src/main/java/com/schdlr/repo/UserManagmentRepo.java
+++ src/main/java/com/schdlr/repo/UserManagmentRepo.java
@@ -1,10 +1,18 @@
 package com.schdlr.repo;
 
+import java.util.Optional;
+
 import com.schdlr.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserManagmentRepo extends JpaRepository<User,Integer>{
+
+    Optional<User> findByUsername(String username);
+
+    User findByPassword(String password);
 
 
 
