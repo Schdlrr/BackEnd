4618f4a0533400dde3dce2347157e0947a2279fc
ErdiSyla <erdisyla6@gmail.com>
2024-11-11 20:07:48 +0100

    Added the bean to create OpenAPI for documentation of Apis

--- src/main/java/com/schdlr/SchdlrApplication.java
+++ src/main/java/com/schdlr/SchdlrApplication.java
@@ -2,9 +2,32 @@ package com.schdlr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class SchdlrApplication {
+
+     @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Schdlr API")
+                        .version("1.0.0")
+                        .description("API documentation for the Schdlr application")
+                        .contact(
+                            new Contact()
+                            .name("Erdi")
+                            .email("erdisyla6@gmail.com"))
+                        .license(
+                        new License()
+                        .name("Apache 2.0")
+                        .url("http://springdoc.org")));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SchdlrApplication.class, args);
