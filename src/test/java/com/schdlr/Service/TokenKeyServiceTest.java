e7f8db02d6e026b3af813f88df27bc1bf6ed62a0
ErdiSyla <erdisyla6@gmail.com>
2025-01-20 20:04:08 +0100

    Commit 96: Added test class for checking if TokenKeyService manages the keys correctly

--- src/test/java/com/schdlr/Service/TokenKeyServiceTest.java
+++ src/test/java/com/schdlr/Service/TokenKeyServiceTest.java
@@ -1,5 +1,6 @@
 package com.schdlr.Service;
 
+import com.schdlr.dto.KeyActivity;
 import com.schdlr.model.TokenKey;
 import com.schdlr.repo.TokenKeyRepo;
 import com.schdlr.service.TokenKeyService;
@@ -12,8 +13,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Objects;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -39,6 +46,27 @@ public class TokenKeyServiceTest {
 
 		verify(mockTokenKeyRepo, times(2)).save(any(TokenKey.class));
 		verify(mockTokenExtractionUtil, times(1)).refreshKeys();
+	}
+
+	@Test
+	void TokenKeyService_testManageKeys_ManagesCorrectly() {
+		TokenKey activeKey = new TokenKey();
+		activeKey.setKeyActivity(KeyActivity.ACTIVE);
+		activeKey.setTimeOfCreation(Instant.now().minus(91,ChronoUnit.DAYS ));
+
+		TokenKey graceKey = new TokenKey();
+		graceKey.setKeyActivity(KeyActivity.ACTIVE);
+		graceKey.setTimeOfCreation(Instant.now().minus(84, ChronoUnit.DAYS));
+
+		given(mockTokenKeyRepo.findAll()).
+				willReturn(List.of(activeKey, graceKey));
+
+		tokenKeyService.manageKeys();
+
+		// Assert
+		verify(mockTokenKeyRepo, times(1)).deleteKeyById(activeKey.getKid());
+		verify(mockTokenKeyRepo, times(1))
+				.save(argThat(k -> Objects.equals(k.getKid(), graceKey.getKid()) && k.getKeyActivity() == KeyActivity.GRACE));
 	}
 
 }
